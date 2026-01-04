511462713d3bef483f9c2c8c9df3339635df4381
raddevus <1radeutsch1@gmail.com>
2026-01-04 16:23:53 -0500

    Adding new files from original CYaPass UWP solution

---
+++ Models/UserPath.cs
@@ -0,0 +1,38 @@
+
+namespace CYaPass_Avalonia.Models;
+
+class UserPath
+{
+  public List<Point> allPoints = new List<Point>();
+  public HashSet<Segment> allSegments = new HashSet<Segment>();
+  private Point currentPoint;
+  public int PointValue;
+  private int previousPostValue;
+
+  public void append(Point currentPoint, int postValue)
+  {
+      this.currentPoint = currentPoint;
+      
+      if (allPoints.Count >= 1)
+      {
+          if (allPoints[allPoints.Count - 1].X == currentPoint.X && allPoints[allPoints.Count - 1].Y == currentPoint.Y)
+          {
+              // user clicked the same point twice
+              return;
+          }
+          allSegments.Add(new Segment(allPoints[allPoints.Count - 1], currentPoint, postValue + previousPostValue));
+      }
+      allPoints.Add(currentPoint);
+      previousPostValue = postValue;
+
+  }
+
+  public void CalculateGeometricValue()
+  {
+      this.PointValue = 0;
+      foreach (Segment s in allSegments)
+      {
+          this.PointValue += s.PointValue;
+      }
+  }
+}

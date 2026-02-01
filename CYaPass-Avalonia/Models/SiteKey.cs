078026097195147f81ccfef6ed72e5b741309711
raddevus <1radeutsch1@gmail.com>
2026-02-01 00:32:58 -0500

    stubbed out SiteKey class - more to come

---
+++ CYaPass-Avalonia/Models/SiteKey.cs
@@ -0,0 +1,8 @@
+
+namespace CYaPass_Avalonia.Models;
+
+public class SiteKey{
+   // notes about structure from JSON
+   // [{"MaxLength":0,"HasSpecialChars":false,"HasUpperCase":false,"Key":"QHRyYW5zZmVyS2V5"}
+   //
+}

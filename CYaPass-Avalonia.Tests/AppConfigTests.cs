c4c4b8324747864dd133019ac866ae678781cbc5
raddevus <1radeutsch1@gmail.com>
2026-02-15 16:15:05 -0500

    Added ReadBasicApConfig test & have that all worked out -- had to add a Assert on exception in case the config couldn't be read.

--- CYaPass-Avalonia.Tests/AppConfigTests.cs
+++ CYaPass-Avalonia.Tests/AppConfigTests.cs
@@ -22,15 +22,34 @@ public class AppConfigTests{
    }
 
    [Fact]
-   async void SaveBasicAppConfig(){
+   async  Task SaveBasicAppConfig(){
 
       AppConfig ac = new(){ LastSelectedKey="lastOne", TransferUrl="https://actionmobile.app/", MultiHash = new AppConfig.MultiHashRecord(true,3)};
       var targetDir = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile);
       var configFile = "cya.config";
+      File.Delete(Path.Combine(targetDir, configFile));
       var output = JsonSerializer.Serialize(ac);
       await File.AppendAllTextAsync(Path.Combine(targetDir,configFile), output);
      if (File.Exists(Path.Combine(targetDir, configFile))){
         Console.WriteLine("Success! Wrote file.");
      }
+   }
+   
+   [Fact]
+   async Task ReadBasicAppConfig(){
+      var targetDir = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile);
+      var configFile = "cya.config";
+      if (File.Exists(Path.Combine(targetDir,configFile))){
+         var configJson = await File.ReadAllTextAsync(Path.Combine(targetDir,configFile));
+         AppConfig? config = null;
+         var ex = Record.Exception(() =>{
+            config  = JsonSerializer.Deserialize<AppConfig>(configJson);
+         });
+         Assert.Null(ex);
+         Console.WriteLine($"last key: {config.LastSelectedKey} transferUrl: {config.TransferUrl}");
+      }
+      else{
+         Console.WriteLine($"Couldn't do the work, because test file doesn't exist: {Path.Combine(targetDir, configFile)}");
+      }
    }
 }

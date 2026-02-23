326b9bdd03a45025bcd530f89fe8212b5c59b60a
raddevus <1radeutsch1@gmail.com>
2026-02-23 15:34:19 -0500

    Added new export msg box so we can get values from user.

---
+++ CYaPass-Avalonia/Views/ExportMsgBox.axaml.cs
@@ -0,0 +1,33 @@
+using Avalonia.Controls;
+using Avalonia.Interactivity;
+using CYaPass_Avalonia.Models;
+
+namespace CYaPass_Avalonia.Views;
+
+public partial class SiteKeyMsgBox : Window
+{
+   public SiteKey SiteKey {get;set;}
+   public SiteKeyMsgBox(): this("default message") {
+   }
+    public SiteKeyMsgBox(string message)
+    {
+        InitializeComponent();
+        MessageText.Text = message;
+        // Sets Focus to the SiteKey text box
+        this.Opened += (_, __) => { SiteKeyText.Focus(); };
+    }
+
+    private void Ok_Click(object? sender, RoutedEventArgs e)
+    {
+       SiteKey = new();
+       // replace all white-space anywhere in string
+       SiteKey.Key = SiteKeyText?.Text?.Replace(" ", "") ?? string.Empty;
+
+       Close(true);
+    }
+
+    private void Cancel_Click(object? sender, RoutedEventArgs e)
+    {
+        Close(false);
+    }
+}

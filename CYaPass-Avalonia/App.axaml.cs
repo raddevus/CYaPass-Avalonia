c264127ff8d019f41ec837e7780ff1136fd6374d
raddevus <1radeutsch1@gmail.com>
2026-08-30 19:53:46 -0400

    Upped version to 1.1.0.0 & added code to display version on title bar of app

--- CYaPass-Avalonia/App.axaml.cs
+++ CYaPass-Avalonia/App.axaml.cs
@@ -6,6 +6,7 @@ using System.Linq;
 using Avalonia.Markup.Xaml;
 using CYaPass_Avalonia.ViewModels;
 using CYaPass_Avalonia.Views;
+using System.Reflection; // added for GetExecutingAssembly
 
 namespace CYaPass_Avalonia;
 
@@ -22,10 +23,18 @@ public partial class App : Application
         {
             // Avoid duplicate validations from both Avalonia and the CommunityToolkit. 
             // More info: https://docs.avaloniaui.net/docs/guides/development-guides/data-validation#manage-validationplugins
+            string version = Assembly.GetExecutingAssembly()
+                .GetCustomAttribute<AssemblyInformationalVersionAttribute>()?
+                .InformationalVersion ?? string.Empty;
+
+            // InformationalVersion has extra stuff in the string we don't want
+            // so we trim it off
+            version = version.Substring(0, version.IndexOf("+"));
             desktop.MainWindow = new MainWindow
             {
                 DataContext = new MainWindowViewModel(),
             };
+            desktop.MainWindow.Title = $"CYaPass - v{version}";
         }
 
         base.OnFrameworkInitializationCompleted();
